9e1dae9f96833adc33080a9c5eaef8c325ac6f3d
SotaTek-HoangNguyen2 <hoang.nguyen2@sotatek.com>
2021-01-27 17:08:37 +0700

    Just testing git commit without predefine the -m message

--- src/employee/decorator/getEmployee.decorator.ts
+++ src/employee/decorator/getEmployee.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Employee } from '../emloyee.entity';
 
+//Decorator to get an emp
 export const getEmployee = createParamDecorator(
   (_: any, ctx: ExecutionContext): Employee => {
     const request = ctx.switchToHttp().getRequest();
