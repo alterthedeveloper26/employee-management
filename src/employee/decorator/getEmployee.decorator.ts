7aa63e14f61d0bf03300e891cbb594905dc68530
SotaTek-HoangNguyen2 <hoang.nguyen2@sotatek.com>
2021-01-28 17:13:59 +0700

    Just testing create-remote-branch

--- src/employee/decorator/getEmployee.decorator.ts
+++ src/employee/decorator/getEmployee.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Employee } from '../emloyee.entity';
 
-//Decorator to get an emp
+//Decorator to get an emp, nothing
 export const getEmployee = createParamDecorator(
   (_: any, ctx: ExecutionContext): Employee => {
     const request = ctx.switchToHttp().getRequest();
